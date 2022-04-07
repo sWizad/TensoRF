2c243031e71cc0f5db72a8581b4d994dda7e006f
Pakkapon Phongthawee <pure.gif@gmail.com>
2022-04-07 10:26:45 +0700

    fix crash when run without tiny-cuda

--- models/pure.py
+++ models/pure.py
@@ -1,6 +1,6 @@
 # a collection of pure's implemented model
-from .hashgrid3d import HashGrid3D
-from .tensorhashgrid import TensorHashGrid
-from .tensoRF_plane_only import TensorRFPlaneOnly   
-from .triplane_classic import TriPlaneClassic
-from .tensorhashgridincrease import TensorHashGridIncrease
+#from .hashgrid3d import HashGrid3D
+#from .tensorhashgrid import TensorHashGrid
+#from .tensoRF_plane_only import TensorRFPlaneOnly   
+#from .triplane_classic import TriPlaneClassic
+#from .tensorhashgridincrease import TensorHashGridIncrease
